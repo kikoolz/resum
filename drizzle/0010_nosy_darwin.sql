caf52e826b4ea6927524c441b4b5fe4edf40e57a
kikoolz <kenlubs45@gmail.com>
2026-08-12 20:24:55 +0300

    Refactor code structure for improved readability and maintainability

---
+++ drizzle/0010_nosy_darwin.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `user_files` ADD `file_data` text;
--- drizzle/0010_rename_r2_to_storage.sql
+++
@@ -1,3 +0,0 @@
--- Rename r2_key column to storage_key and add file_data column
-ALTER TABLE user_files RENAME COLUMN r2_key TO storage_key;
-ALTER TABLE user_files ADD COLUMN file_data TEXT;
